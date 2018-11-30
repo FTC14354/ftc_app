896ae7221fe2ccec49e7bfcca2a179b3ed69c0cc
Daniel Shookowsky <dshookowsky@gmail.com>
2018-11-29 23:03:46 -0500

    Code reformatted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/LowerFromLander.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/LowerFromLander.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.modules;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.DriveStyle;
 
 import static java.lang.Thread.sleep;
@@ -15,27 +16,30 @@ public class LowerFromLander {
     private final static int encoder_ticks_top_released = 18985;
     private final static double POWER_MAX = 0.7;
 
-    public LowerFromLander (HardwareMap hardwareMap, DriveStyle drivestyle) {
+    public LowerFromLander(HardwareMap hardwareMap, DriveStyle drivestyle) {
         this.hardwareMap = hardwareMap;
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
         this.driveStyle = drivestyle;
     }
-    public void landRobot () {
+
+    public void landRobot() {
         lowerRobot();
         driveForward();
         lowerArm();
     }
-    public void lowerRobot (){
-        moveMotorToPosition(liftMotor, encoder_ticks_top_released,Direction.FORWARD);
+
+    public void lowerRobot() {
+        moveMotorToPosition(liftMotor, encoder_ticks_top_released, Direction.FORWARD);
+    }
+
+    public void driveForward() {
+        driveStyle.setDriveValues(POWER_MAX, POWER_MAX);
+        sleep(1000);
+        driveStyle.stop();
 
     }
-    public void driveForward () {
-driveStyle.setDriveValues (POWER_MAX,POWER_MAX);
-sleep(1000);
-driveStyle.stop();
 
-    }
-    public void lowerArm(){
+    public void lowerArm() {
         moveMotorToPosition(liftMotor, encoder_ticks_top_released, Direction.REVERSE);
 
 
@@ -46,11 +50,12 @@ driveStyle.stop();
         liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         liftMotor.setTargetPosition(position);
-        while (liftMotor.isBusy()){
+        while (liftMotor.isBusy()) {
 
         }
         liftMotor.setPower(0);
     }
+
     public final void sleep(long milliseconds) {
         try {
             Thread.sleep(milliseconds);
@@ -58,5 +63,4 @@ driveStyle.stop();
             Thread.currentThread().interrupt();
         }
     }
-
 }
